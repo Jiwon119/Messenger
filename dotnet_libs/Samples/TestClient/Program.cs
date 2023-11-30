77a5720e0105b59ff481f2a406fd8155383e82d7
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2023-11-30 20:00:09 +0900

    파일 정리

--- dotnet_libs/Samples/TestClient/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TestClient;
-
-Console.WriteLine("Hello, World!");
-
-ClientRuntime rt = new ClientRuntime();
-rt.InitRuntime();
-rt.Run();
-rt.Stop();
--- dotnet_libs/Samples/TestServer/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TestServer;
-
-Console.WriteLine("Hello, World!");
-
-var rt = new ServerRuntime();
-rt.InitRuntime();
-rt.Run();
-rt.Stop();
